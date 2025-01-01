0acba85aeb434bf7a008029087ae4a739a612019
MichaelJuannn <juan600d@gmail.com>
2025-01-01 21:40:14 +0700

    use fzf instead telescope

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -39,10 +39,7 @@ return {
           },
         },
       }
-      --  This function gets run when an LSP attaches to a particular buffer.
-      --    That is to say, every time a new file is opened that is associated with
-      --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
-      --    function will be executed to configure the current buffer
+
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
         callback = function(event)
@@ -56,37 +53,13 @@ return {
             vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
           end
 
-          -- Jump to the definition of the word under your cursor.
-          --  This is where a variable was first declared, or where a function is defined, etc.
-          --  To jump back, press <C-t>.
           map('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-
-          -- Find references for the word under your cursor.
           map('gr', vim.lsp.buf.references, '[G]oto [R]eferences')
-
-          -- Jump to the implementation of the word under your cursor.
-          --  Useful when your language has ways of declaring types without an actual implementation.
           map('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-
-          -- Jump to the type of the word under your cursor.
-          --  Useful when you're not sure what type a variable is and you want to see
-          --  the definition of its *type*, not where it was *defined*.
           map('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-
-          -- Fuzzy find all the symbols in your current document.
-          --  Symbols are things like variables, functions, types, etc.
           map('<leader>ds', vim.lsp.buf.document_symbol, '[D]ocument [S]ymbols')
-
-          -- Fuzzy find all the symbols in your current workspace.
-          --  Similar to document symbols, except searches over your entire project.
           map('<leader>ws', vim.lsp.buf.workspace_symbol, '[W]orkspace [S]ymbols')
-
-          -- Rename the variable under your cursor.
-          --  Most Language Servers support renaming across files, etc.
-          map('<leader>cr', vim.lsp.buf.rename, '[R]e[n]ame')
-
-          -- Execute a code action, usually your cursor needs to be on top of an error
-          -- or a suggestion from your LSP for this to activate.
+          map('<leader>cr', vim.lsp.buf.rename, 'Lsp Rename')
           map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
 
           map('<leader>cA', function()
@@ -103,11 +76,6 @@ return {
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
-          -- The following two autocommands are used to highlight references of the
-          -- word under your cursor when your cursor rests there for a little while.
-          --    See `:help CursorHold` for information about when this is executed
-          --
-          -- When you move your cursor, the highlights will be cleared (the second autocommand).
           local client = vim.lsp.get_client_by_id(event.data.client_id)
           if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
             local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
@@ -144,23 +112,19 @@ return {
         end,
       })
 
-      -- Change diagnostic symbols in the sign column (gutter)
       if vim.g.have_nerd_font then
-        -- Define your diagnostic icons
         local signs = {
-          ERROR = '', -- Error icon
-          WARN = '', -- Warning icon
-          INFO = '', -- Info icon
-          HINT = '', -- Hint icon
+          ERROR = '',
+          WARN = '',
+          INFO = '',
+          HINT = '',
         }
         local diagnostic_signs = {}
 
-        -- Create a table for diagnostic signs
         for type, icon in pairs(signs) do
           diagnostic_signs[vim.diagnostic.severity[type]] = icon
         end
 
-        -- Configure diagnostics
         vim.diagnostic.config {
           underline = true,
           update_in_insert = false,
@@ -195,12 +159,7 @@ return {
         }
       end
 
-      -- LSP servers and clients are able to communicate to each other what features they support.
-      --  By default, Neovim doesn't support everything that is in the LSP specification.
-      --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
-      --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
-      --  NOTE: THIS IS USING BLINK CMP NOT NVIM CMP
-
+      --  NOTE: THIS IS USING BLINK CMP NOT NVIM CMP for capabilities
       local capabilities = require('blink.cmp').get_lsp_capabilities()
       local lspconfig = require 'lspconfig'
       for server, config in pairs(servers) do
